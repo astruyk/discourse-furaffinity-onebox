e2d0b7354c4bec413447042e2667dc2487ad0945
Anton Struyk <astruyk@gmail.com>
2018-03-22 15:40:18 -0400

    Use xpath since 'contains' is not valid CSS selector syntax. Printf debugging FTW!

--- plugin.rb
+++ plugin.rb
@@ -25,7 +25,7 @@ class Onebox::Engine::TwitchStreamOnebox
 		doc = Nokogiri::HTML(open(@url));
 		begin
 			title = doc.css("title")[0].text;
-			submissionUrl = doc.css("a:contains('Download')[href]")[0]["href"];
+			submissionUrl = doc.xpath("//a[contains(text(), 'Download')]")[0]["href"];
 			#submissionAuthor = doc.css("...");
 			#submissionTitle = doc.css("...")
 			html.push(title);
