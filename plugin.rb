61ac26bde4f679a7aa3b5cca34b02c8eb77e5b71
Anton Struyk <astruyk@gmail.com>
2018-03-22 13:42:01 -0400

    Initial test checkin. Try to extract title of submission page.

---
+++ plugin.rb
@@ -0,0 +1,28 @@
+# name: Discourse FurAffinity Onebox
+# about: Adds support for properly embedding Furaffinity submissions as OneBox items in Discourse
+# version: 1.0
+# authors: tfProxy
+# url: https://github.com/tfProxy/discourse-furaffinity-onebox
+
+# Onebox for Furaffinity submissions.
+class Onebox::Engine::TwitchStreamOnebox
+	include Onebox::Engine
+
+	# Example submission URL is https://www.furaffinity.net/view/10235836/
+	REGEX = /^https?:\/\/(?:www\.)?furaffinity\.net\/view\/([0-9]{4,25})(?:\/)?$/
+	matches_regexp REGEX
+
+	def submissionId
+		@url.match(REGEX)[1]
+	end
+	
+	def to_html
+		doc = Nokogiri::HTML(open(@url));
+		title = doc.css("title")[0].text;
+		html = [];
+		html.push("<div>");
+		html.push(title);
+		html.push("</div>");
+		html.join('');
+	end
+end
