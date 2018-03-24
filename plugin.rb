89879299a2f0ab110e4b99e5578419ef6f513f20
Anton Struyk <astruyk@gmail.com>
2018-03-24 19:38:14 -0400

    Try extending the standard embed item again to avoid having to parse out the twitter data ourselves. Also preformat errors.

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@ register_asset "styles.css"
 
 # Onebox for Furaffinity submissions.
 class Onebox::Engine::TwitchStreamOnebox
-	include Onebox::Engine
+	include Onebox::Engine::StandardEmbed
 
 	# Example submission URL is https://www.furaffinity.net/view/10235836/
 	REGEX = /^https?:\/\/(?:www\.)?furaffinity\.net\/view\/([0-9]{4,25})(?:\/)?$/
@@ -19,56 +19,48 @@ class Onebox::Engine::TwitchStreamOnebox
 		html.push("<div class=\"fa_container\" >");
 		begin
 			twitter = get_twitter;
-			titleElements = twitter["title"];
-			descriptionElements = twitter["description"];
-			imageElements = twitter["image"];
+			title = twitter["title"];
+			description = twitter["description"];
+
+			# Note: The name in this URL has to match the name declared at the top of this file.
+			imageUrl = twitter["image"] || "/plugins/discourse-furaffinity-onebox/images/fa_logo.png";
+
+			# Don't generate an 800px image, generate a smaller one.
+			imageUrl.sub("@800-", "@400-");
 
 			# Most pages (even the NSFW blocked ones) will have this.
 			# If not, we're not on a FA page we can do anything
 			# useful with and we can just bail.
-			if !titleElements.blank?
-				title = titleElements[0]["content"];
-				html.push("<a href=\"#{@url}\">");
+			html.push("<a href=\"#{@url}\">");
+			if !title.nil?
 				html.push("<div class=\"fa_title\">");
 				html.push(title);
 				html.push("</div>");
-
-				# Note: The name in this URL has to match the name declared at the
-				# top of this file.
-				imageUrl = "/plugins/discourse-furaffinity-onebox/images/fa_logo.png";
-				if !imageElements.blank?
-					# If we have a better URL for the image, it must be SFW. Use it.
-					imageUrl = imageElements[0]["content"];
-					
-					# Don't generate an 800px image, generate a smaller one.
-					imageUrl.sub("@800-", "@400-");
-				end
-				html.push("<div class=\"fa_image\">")
-				html.push("<img src=\"#{imageUrl}\" />");
-				html.push("</div>");
-
-				if !descriptionElements.blank?
-					description = descriptionElements[0]["content"];
-					html.push("<div class=\"fa_description\">");
-					html.push(description);
-					html.push("</div>");
-				end
-
-				if imageElements.blank?
-					html.push("<div class=\"fa_nsfw_warning\">");
-					html.push("(The image preview could not be retrieved, most likely because it is adult-only)");
-					html.push("</div>");
-				end
-
-				html.push("</a>");
-			else
-				# Bail! Nothing we can do. Maybe someone else can generate something useful for this.
-				return;
 			end
+
+			html.push("<div class=\"fa_image\">")
+			html.push("<img src=\"#{imageUrl}\" />");
+			html.push("</div>");
+
+			if description.nil?
+				html.push("<div class=\"fa_description\">");
+				html.push(description);
+				html.push("</div>");
+			end
+
+			if twitter["image"].nil?
+				html.push("<div class=\"fa_nsfw_warning\">");
+				html.push("(The image preview could not be retrieved, most likely because it is adult-only)");
+				html.push("</div>");
+			end
+
+			html.push("</a>");
 		rescue StandardError => error
+			html.push("<pre>");
 			html.push(error.message);
 			html.push("<br/><br/>");
 			html.push(error.backtrace);
+			html.push("</pre>");
 		end
 		html.push("</div>");
 		html.join('');
