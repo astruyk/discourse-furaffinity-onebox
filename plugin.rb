6338ce1185c57e291f53af0226760efd0d0396a3
Anton Struyk <astruyk@gmail.com>
2018-03-24 16:37:44 -0400

    Ditch 'em

--- plugin.rb
+++ plugin.rb
@@ -9,8 +9,6 @@ register_asset "styles.css"
 # Onebox for Furaffinity submissions.
 class Onebox::Engine::FuraffinityOnebox
 	include Onebox::Engine
-	include Onebox::LayoutSupport
-	include Onebox::HTML
 
 	# Example submission URL is https://www.furaffinity.net/view/10235836/
 	REGEX = /^https?:\/\/(?:www\.)?furaffinity\.net\/view\/([0-9]{4,25})(?:\/)?$/
