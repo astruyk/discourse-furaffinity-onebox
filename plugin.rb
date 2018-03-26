bdc65bd698af045040e4d336d906520334ce4589
Anton Struyk <astruyk@gmail.com>
2018-03-26 12:56:45 -0400

    Try something simple.

--- plugin.rb
+++ plugin.rb
@@ -9,18 +9,16 @@ register_asset "styles.css"
 # Onebox for Furaffinity submissions.
 class Onebox::Engine::FuraffinityOnebox
 	include Onebox::Engine
-	include Onebox::LayoutSupport
 
 	# Example submission URL is https://www.furaffinity.net/view/10235836/
 	REGEX = /^https?:\/\/(?:www\.)?furaffinity\.net\/view\/([0-9]{4,25})(?:\/)?$/
 	matches_regexp REGEX
 
-	def data
-		{
-			url: @url,
-			name: "Foo",
-			image: "Bar",
-			description: "Test"
-		}
+	def to_html
+		<<-HTML
+          <div placeholder>
+            <p>LINK to <a href="#{@url}">#{@url}</a></p>
+          </div>
+        HTML
 	end
 end
