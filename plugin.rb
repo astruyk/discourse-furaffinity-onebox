8b4c58c056b49be623650bd0eef5500144459654
Anton Struyk <astruyk@gmail.com>
2018-03-26 12:31:45 -0400

    Maybe this isn't a onebox thing?

--- plugin.rb
+++ plugin.rb
@@ -9,7 +9,7 @@ register_asset "styles.css"
 # Onebox for Furaffinity submissions.
 class Onebox::Engine::FuraffinityOnebox
 	include Onebox::LayoutSupport
-	include Onebox::HTML
+	include HTML
 
 	# Example submission URL is https://www.furaffinity.net/view/10235836/
 	REGEX = /^https?:\/\/(?:www\.)?furaffinity\.net\/view\/([0-9]{4,25})(?:\/)?$/
