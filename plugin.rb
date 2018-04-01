3f94acc2e98fccbd2b2e7325e9bed4e22f56c167
Anton Struyk <astruyk@gmail.com>
2018-03-31 20:21:44 -0400

    Nothing works ATM. use the default logo for all things.

--- plugin.rb
+++ plugin.rb
@@ -39,27 +39,27 @@ class Onebox::Engine::FuraffinitySubmissionOnebox
 			# call it with those default values w/o actually visiting the link in a real
 			# browser first. Thanks FA.
 			# imageElements = doc.css("meta[property='og:image:secure_url]");
-			mimeType = "img/jpeg";
-			imgBase64 = "";
-			if !imageElements.blank?
-				imageUrl = imageElements[0]["content"];
-				imageUrl = imageUrl.sub("@800-", "@100-");
-				imgBase64 = imageUrl;
+			# mimeType = "img/jpeg";
+			# imgBase64 = "";
+			# if !imageElements.blank?
+			# 	imageUrl = imageElements[0]["content"];
+			# 	imageUrl = imageUrl.sub("@800-", "@100-");
+			# 	imgBase64 = imageUrl;
 
-				# Actually try to open the URL so that the thumbnail will be generated (if it isn't already).
-				# Use headers ripped from a normal browser session so that it won't trigger the hotlinking errors.
-				# imgData = open(imageUrl,
-				# 		"Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-				# 		"Accept-Encoding" => "gzip, deflate",
-				# 		"Accept-Language" => "n-GB,en-US;q=0.9,en;q=0.8",
-				# 		"Connection" => "keep-alive",
-				# 		"Host" => "d.facdn.net",
-				# 		"Upgrade-Insecure-Requests" => "1",
-				# 		"User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
-				# 	);
-				# mimeType = imgData.content_type;
-				# imgBase64 = ::Base64.encode64(imgData.read);				
-			end
+			# 	# Actually try to open the URL so that the thumbnail will be generated (if it isn't already).
+			# 	# Use headers ripped from a normal browser session so that it won't trigger the hotlinking errors.
+			# 	# imgData = open(imageUrl,
+			# 	# 		"Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+			# 	# 		"Accept-Encoding" => "gzip, deflate",
+			# 	# 		"Accept-Language" => "n-GB,en-US;q=0.9,en;q=0.8",
+			# 	# 		"Connection" => "keep-alive",
+			# 	# 		"Host" => "d.facdn.net",
+			# 	# 		"Upgrade-Insecure-Requests" => "1",
+			# 	# 		"User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+			# 	# 	);
+			# 	# mimeType = imgData.content_type;
+			# 	# imgBase64 = ::Base64.encode64(imgData.read);				
+			# end
 		rescue StandardError => err
 			title = "Error";
 			description = err.message + "\n\n" + err.backtrace;
