9ac6aa93eb329a9d002fd56ccb29f286a91a05cc
Anton Struyk <astruyk@gmail.com>
2018-04-03 16:25:26 -0400

    Turn back on FA image previews with horrendous hack. Also fix bad error handling code that was causing messages to be surpressed when things went wrong.

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,8 @@
 # authors: Anton Struyk
 # url: https://github.com/astruyk/discourse-furaffinity-onebox
 
+require 'base64'
+
 # Onebox for Furaffinity submissions.
 class Onebox::Engine::FuraffinitySubmissionOnebox
 	include Onebox::Engine
@@ -16,7 +18,7 @@ class Onebox::Engine::FuraffinitySubmissionOnebox
 		linkUrl = @url;
 		title = "FurAffinity Submission";
 		description = "";
-		imageUrl = "https://www.furaffinity.net/themes/classic/img/banners/fa_logo.png";
+		imageSrc = "https://www.furaffinity.net/themes/classic/img/banners/fa_logo.png";
 		iconUrl = "https://www.furaffinity.net/themes/classic/img/favicon.ico";
 
 		begin
@@ -31,17 +33,30 @@ class Onebox::Engine::FuraffinitySubmissionOnebox
 				description = descriptionElements[0]["content"];
 			end
 
-			# We're using the actual source image here (not the og:image value) because
-			# FA's hotlinking protection causes us to generate 403 errors if we try to
-			# call it with those default values w/o actually visiting the link in a real
-			# browser first. Thanks FA.
-			# imageElements = doc.css("#submissionImg");
-			# if !imageElements.blank?
-			# 	imageUrl = imageElements[0]["src"];
-			# end
+			imageElements = doc.css("meta[property='og:image:secure_url']");
+			if !imageElements.blank?
+				# Try to down-size the default thumbnail size (800px) to something
+				# more reasonable, since we're not embedding the whole image anyway.
+				thumbnailUrl = imageElements[0]["content"].sub("@800-", "@200-");
+
+				# Read the data from the URL. This prevents images that haven't been visited
+				# in a long time from showing up as broken links. FA is doing some kind of crazy
+				# server-side caching for thumbnails and not generating them when requested by
+				# an <img /> element (returning 407 errors instead), it works if someone
+				# visits the URL and pulls the actual image data though... 
+				# This seems like a bug with their hotlinking protection because they actaully
+				# embedd this metadata for tools to use as thumbnails. Lousy.
+				begin
+					imageBase64Data = ::Base64.encode64(open(thumbnailUrl) { |io| io.read });
+					imageSrc = thumbnailUrl;
+				rescue StandardError => err
+					# Assuming the error came from reading the data, just use the default FA URL
+					# and surpress the error.
+				end
+			end
 		rescue StandardError => err
 			title = "Error";
-			description = err.message + "\n\n" + err.backtrace;
+			description = err.message + "\n\n" + "<pre>" + err.backtrace.join("\n") + "</pre>";
 		end
 
 		<<-HTML
@@ -51,7 +66,7 @@ class Onebox::Engine::FuraffinitySubmissionOnebox
 					<a href="#{linkUrl}" target="_blank" rel="nofollow noopener">furAffinity.net</a>
 				</header>
 				<article class="onebox-body">
-					<img src="#{imageUrl}" class="thumbnail size-resolved" />
+					<img src="#{imageSrc}" class="thumbnail" />
 					<h3><a href="#{linkUrl}" target="_blank" rel="nofollow noopener">#{title}</a></h3>
 					<p>#{description}</p>
 					<div style="clear: both"></div>
